eb97acea00333f29ca56ca91ad4d23dc576773b0
StaceyO21 <staceyobrien21@gmail.com>
2025-06-10 19:41:50 -0500

    data types

--- BethanyPieShopHRM/BethanyPieShopHRM/Program.cs
+++ BethanyPieShopHRM/BethanyPieShopHRM/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+// byte numberOfEmployees = 300;
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+hoursWorked = 148;
+
+//monthlyWage = true;
+
+const double interestRate = 0.07;
